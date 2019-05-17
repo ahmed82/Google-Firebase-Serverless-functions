287221adb8b430e1a4be5534c28584a35df64895
Ahmed AL Salih <a.alsalih2@gmail.com>
2019-05-17 18:34:32 -0400

    add Google Function to Create User wich is Phone and request the pasword and send a text meesage for verification with verify the password

--- functions/index.js
+++ functions/index.js
@@ -3,16 +3,18 @@ const functions = require('firebase-functions');
 const createUser = require('./create_user');
 const serviceAccount = require('./service_account.json');
 const requestOneTimePassword = require('./request_one_time_password');
+const verrifyOneTimePassword = require('./verify_one_time_password');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 /*  exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
  }); */
- admin.initializeApp({
+admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://one-time-password-f996d.firebaseio.com"
-  });
+});
 
- exports.createUser = functions.https.onRequest(createUser);
- exports.requestOneTimePassword = functions.https.onRequest(requestOneTimePassword);
+exports.createUser = functions.https.onRequest(createUser);
+exports.requestOneTimePassword = functions.https.onRequest(requestOneTimePassword);
+exports.verrifyOneTimePassword = functions.https.onRequest(verrifyOneTimePassword);
---
+++ functions/verify_one_time_password.js
@@ -0,0 +1,32 @@
+const admin = require('firebase-admin');
+
+module.exports = function (req, res) {
+
+    if (!req.body.phone || req.body.code) {
+        return res.status(422).send({ error: 'Phone and code must be provideds' });
+    }
+
+    const phone = String(req.body.phone).replace(/[^\d]/g, '');
+    const code = parseInt(req.body.code);
+
+    admin.auth().getUser(phone)
+        .then(() => {
+            //admin.database().ref('users/' + phone).on('value', snapshot => {
+            const ref = admin.database().ref('users/' + phone);
+            ref.on('value', snapshot => {
+                ref.off(); // to Stop Listening.
+                const user = snapshot.val();
+
+                if (user.code !== code || !user.codeValid) {
+                    return res.status(422).send({ error: 'code not valid' });
+                }
+                ref.update({ codeValid = false });
+                admin.auth().createCustomToken(phone)
+                    .then(token => res.send({Token : token}))
+
+            });
+
+        })
+        .catch((err) => res.status(422).send({ error: err || 'User not found' }))
+
+}
